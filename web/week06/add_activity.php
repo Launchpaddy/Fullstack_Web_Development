a76a412cca65f77010e309648d6b937dde4b30d9
trevik <pet16024@byui.edu>
2018-10-27 19:47:15 -0600

    add activity dang it

--- web/week06/add_activity.php
+++ web/week06/add_activity.php
@@ -5,19 +5,21 @@ require('dbConnect.php');
 $db = get_db();
 
 $activity = htmlspecialchars($_POST['activity']);
-$date     = htmlspecialchars($_POST['date']);
-$place    = htmlspecialchars($_POST['place']);
-$duration = htmlspecialchars($_POST['duration']);
-$quality  = htmlspecialchars($_POST['quality']);
-$id       = htmlspecialchars($_POST['id']);
+// $date     = htmlspecialchars($_POST['date']);
+// $place    = htmlspecialchars($_POST['place']);
+// $duration = htmlspecialchars($_POST['duration']);
+// $quality  = htmlspecialchars($_POST['quality']);
+// $id       = htmlspecialchars($_POST['id']);
 
 
 
  $query = "INSERT INTO activities (name, day, place, hour_duration, inviroment_quality, sport_id) VALUES (:name, :day, :place, :duration, :quality, :id);";
 
+ $query2 "INSERT INTO activities (name) VALUES (:name);"
 
 
-$stmt = $db->prepare($query);
+
+$stmt = $db->prepare($query2);
 
 $stmt->bindValue(":name",     $activity, PDO::PARAM_STR);
 $stmt->bindValue(":day",      $date, PDO::PARAM_STR);
@@ -34,7 +36,7 @@ $stmt->execute();
 
 header('location:activities.php');
 //header("location:$newpage");
-die();
+// die();
 
 
 ?>
