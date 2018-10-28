8358d410a029b0bfee0b7a9deff0b5ca169f717d
trevik <pet16024@byui.edu>
2018-10-27 20:05:21 -0600

    trying again

--- web/week06/add_activity.php
+++ web/week06/add_activity.php
@@ -15,21 +15,21 @@ $activity = htmlspecialchars($_POST['activity']);
 
  // $query = "INSERT INTO activities (name, day, place, hour_duration, inviroment_quality, sport_id) VALUES (:name, :day, :place, :duration, :quality, :id);";
 
- $query2 = "INSERT INTO activities (name, sport_id) VALUES (:name, :id);"
+//  $query2 = "INSERT INTO activities (name, sport_id) VALUES (:name, :id);"
 
 
 
-$stmt = $db->prepare($query2);
+// $stmt = $db->prepare($query2);
 
-$stmt->bindValue(":name", $activity, PDO::PARAM_STR);
-// $stmt->bindValue(":day",      $date, PDO::PARAM_STR);
-// $stmt->bindValue(":place",    $place, PDO::PARAM_STR);
-// $stmt->bindValue(":duration", $duration, PDO::PARAM_INT);
-// $stmt->bindValue(":quality",  $quality, PDO::PARAM_INT);
- $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+// $stmt->bindValue(":name", $activity, PDO::PARAM_STR);
+// // $stmt->bindValue(":day",      $date, PDO::PARAM_STR);
+// // $stmt->bindValue(":place",    $place, PDO::PARAM_STR);
+// // $stmt->bindValue(":duration", $duration, PDO::PARAM_INT);
+// // $stmt->bindValue(":quality",  $quality, PDO::PARAM_INT);
+//  $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
 
-$stmt->execute();
+// $stmt->execute();
 
 
 
