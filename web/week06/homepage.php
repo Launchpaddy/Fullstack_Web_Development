6a56c9df8d3f447846e058534fd38e30dcad15d5
trevik peterson <trevikp@gmail.com>
2018-11-01 12:12:27 -0600

    talking a look at my code and making a change'

--- web/week06/homepage.php
+++ web/week06/homepage.php
@@ -13,20 +13,8 @@
    else
    {
       // we should be in the session. if we are not we probaly want to redirect to login
-
-      // $perfUser = $_POST['username'];
-      // $perfPass = $_POST['password'];
-      // $perfDName = $_POST['displayName'];
-
-      // $username = htmlspecialchars($_POST['username']);
-      // $_SESSION['username'] = $username;
-
-      // $password = htmlspecialchars($_POST['password']);
-      // $_SESSION['password'] = $password;
-
-      // $displayName = htmlspecialchars($_POST['display_name']);
-      // $_SESSION['displayName'] = $displayName;
-      // $_SESSION['logedIn'] = true;
+      header('Location: login.php');
+      die();
    }
 
 
@@ -39,20 +27,12 @@
    $stmt = $db->prepare('SELECT name, id FROM sports WHERE user_id=:user_id');
 
    $stmt->bindvalue(':user_id', $user_id);
-   $stmt->execute();
+   $stmt->execute(); // this needs to be before the fetchAll statement
+
    $sports = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-   //$row = $stmt->fetch();
 
-   //$userDisplayName = $row['display_name'];
-   // sports table
-   // $stmt2 = $d2->prepare('SELECT name, user_id, id FROM sports');
-   // $stmt2->execute();
-   // $sports = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-
-   // activites table
-
-     echo "<h1>user_id: $user_id </h1>";
+  // echo "<h1>user_id: $user_id </h1>";
 
 ?>
 
@@ -63,40 +43,6 @@
 
 </head>
 <body>
-<?php
-   //  foreach ($users as $user) {
-   //    $p  = $user['password'];
-   //    $u  = $user['username'];
-   //    $d  = $user['display_name'];
-   //    if ($username == $u && $password == $p) {
-   //       // echo "<h1>$d's Home Page</h1>";
-   //       $masterUser = $user;
-   //    }
-   // }
-
-   // $p  = $masterUser['password'];
-   // $u  = $masterUser['username'];
-   // $d  = $masterUser['display_name'];
-   // $masterId = $masterUser['id'];
-   echo "<h1>$display_name's PERFORM Home Page</h1>";
-   echo "<h2>Sports</h2>";
-   echo " <h3>username: $username</h3>
-   <h3>password: $password</h3>
-   <h3>display name: $display_name</h3>
-   <h3>user ID: $user_id</h3>";
-    echo "<ul>";
-   foreach ($sports as $sport) {
-       $sport_name = $sport['name'];
-      // $j = $sport['user_id'];
-       $sport_id = $sport['id'];
-      // if ($masterId == $j) {
-         // $_SESSION['sportId'] = $sid;
-         echo "<li><a href=\"activities.php?sport_id=$sport_id\">$sport_name</a></li>";
-     // }
-   }
-    echo "</ul>";
- ?>
-
  <form action="add_sport.php" method="POST">
     <div class="container">
 
@@ -112,6 +58,20 @@
   </div>
 
  </form>
+<?php
+
+   echo "<h1>$display_name's PERFORM Home Page</h1>";
+   echo "<h2>Sports</h2>";
+  echo "<ul>";
+   foreach ($sports as $sport) {
+       $sport_name = $sport['name'];
+       $sport_id = $sport['id'];
+       echo "<li><a href=\"activities.php?sport_id=$sport_id\">$sport_name</a></li>";
+
+   }
+  echo "</ul>";
+ ?>
+
 
 </body>
 </html>
