d084e0827a9cf243082e13198ba18b7356c2a177
trevik peterson <trevikp@gmail.com>
2018-11-01 13:35:18 -0600

    talking a look at my code and making a change'

--- web/week06/add_activity.php
+++ web/week06/add_activity.php
@@ -39,9 +39,9 @@ $stmt->bindValue(":sport_id", $sport_id, PDO::PARAM_INT);
 $stmt->execute();
 
 // now we need to get the id back from the activity we just created
-// $stmt1 = $db->perpare('SELECT id FROM activities WHERE sport_id=$sport_id');
+$stmt = $db->perpare('SELECT id FROM activities WHERE sport_id=$sport_id');
 //$stmt1->bindValue(":s_id", $sport_id, PDO::PARAM_INT);
-// $stmt1->execute();
+$stmt->execute();
 
 // $activity_id = $stmt1->fetch();
 
