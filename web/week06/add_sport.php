2df18dc8c20a00658046b48cd76b5fa14b4e0539
trevik peterson <trevikp@gmail.com>
2018-11-01 12:21:34 -0600

    talking a look at my code and making a change'

--- web/week06/add_sport.php
+++ web/week06/add_sport.php
@@ -1,31 +1,22 @@
 <?php
+
 session_start();
 
 require('dbConnect.php');
 $db = get_db();
 
-$sport   = $_POST['sport'];
-$user_id = $_POST['user_id'];
+$sport   = htmlspecialchars($_POST['sport']);
+$user_id = htmlspecialchars($_POST['user_id']);
 
-
-
- $query = "INSERT INTO sports(name, user_id) VALUES (:sport, :user_id);";
-
-
+$query = "INSERT INTO sports(name, user_id) VALUES (:sport, :user_id);";
 
 $stmt = $db->prepare($query);
 $stmt->bindValue(":sport", $sport, PDO::PARAM_STR);
 $stmt->bindValue(":user_id", $user_id, PDO::PARAM_INT);
 
-
 $stmt->execute();
 
-
-
-
 header('location:homepage.php');
-//header("location:$newpage");
-//die();
-
+die();
 
  ?>
