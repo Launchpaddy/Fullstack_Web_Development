8f21020a83fec2e01fc4a3e2a964c8814491a8c5
trevik peterson <trevikp@gmail.com>
2018-11-01 19:36:46 -0600

    things are going good but i cant do two things in a db for some reason

--- web/week06/activities.php
+++ web/week06/activities.php
@@ -39,7 +39,7 @@ session_start();
    $stmt->execute();
 
    $activities = $stmt->fetchAll(PDO::FETCH_ASSOC);
-   $activity = $stmt->fetch();
+  // $activity = $stmt->fetch());
 
    // // performance table
 
@@ -62,7 +62,7 @@ session_start();
 
   // echo "<li><p> Playing: $s.  $sid</p></li>";
    // loop for activities
-   // foreach ($activities  as $activity) {
+   foreach ($activities  as $activity) {
       $a_name = $activity['name'];
       $a_day = $activity['day'];
       $a_id  = $activity['id'];
@@ -93,7 +93,7 @@ session_start();
 
 
 
-   // }
+   }
 
 
 ?>
