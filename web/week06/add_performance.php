dc75d23480af7449eeca0751bf0d9361eb4a87a2
trevik peterson <trevikp@gmail.com>
2018-11-05 21:07:36 -0700

    changing it to make it work

--- web/week06/add_performance.php
+++ web/week06/add_performance.php
@@ -9,7 +9,7 @@ $db = get_db();
  $performance = htmlspecialchars($_GET['performance']);
  $fun         = htmlspecialchars($_GET['fun']);
  $health      = htmlspecialchars($_GET['health']);
-$activity_id = htmlspecialchars($_GET['activity_id']);
+ $activity_id = htmlspecialchars($_GET['activity_id']);
 
 
  $sport_id = $_GET['sport_id'];
@@ -47,7 +47,7 @@ $stmt->bindValue(":name",         $name, PDO::PARAM_STR);
 $stmt->bindValue(":performance",  $performance, PDO::PARAM_INT);
 $stmt->bindValue(":fun",          $fun,    PDO::PARAM_INT);
 $stmt->bindValue(":health",       $health, PDO::PARAM_INT);
-$stmt->bindValue(":activity_id",  $activitie_id,  PDO::PARAM_INT);
+$stmt->bindValue(":activity_id",  $activity_id,  PDO::PARAM_INT);
 
 //  echo "$id";
 
