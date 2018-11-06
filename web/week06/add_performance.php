8014b44240314c3c94f9883bc13870da16af5f5d
trevik peterson <trevikp@gmail.com>
2018-11-05 20:49:04 -0700

    changing it to make it work

--- web/week06/add_performance.php
+++ web/week06/add_performance.php
@@ -39,7 +39,7 @@ $performances = $stmt1->fetchAll(PDO::FETCH_ASSOC);
 
 
 foreach ($activities as $activity) {
-   echo $activity['id'];
+   echo $activity;
    foreach ($performances as $performance ) {
 
       if ($activity['id'] == $performance['activitie_id']) {
