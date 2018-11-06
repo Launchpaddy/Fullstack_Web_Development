c66b7339f761a37f19c367ce1b1641096099486d
trevik peterson <trevikp@gmail.com>
2018-11-05 18:59:34 -0700

    changing it to make it work

--- web/week06/add_performance.php
+++ web/week06/add_performance.php
@@ -17,17 +17,17 @@ $db = get_db();
 
  //echo $sport_id;
 
-// $query = 'SELECT * FROM activities WHERE sport_id=:s_id';
+// // $query = 'SELECT * FROM activities WHERE sport_id=:s_id';
 
-$stmt = $db->perpare('SELECT * FROM activities WHERE sport_id=$sport_id;');
+// $stmt = $db->perpare('SELECT * FROM activities WHERE sport_id=$sport_id;');
 
-//$stmt->bindValue(':s_id', $sport_id);
-//$stmt1->bindValue(":activity_name", $activity, PDO::PARAM_STR);
+// //$stmt->bindValue(':s_id', $sport_id);
+// //$stmt1->bindValue(":activity_name", $activity, PDO::PARAM_STR);
 
 
-$stmt->execute();
+// $stmt->execute();
 
-$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
